e75991c5572a790860fa22d13f194e684ee165ba
ayush01-dev <111ayushh@gmail.com>
2025-08-15 21:36:14 +0530

    Refactor Fibonacci function to correct recursive relation and improve structure; add new Merge Sort implementation

--- Recursion/practice/finonacci.cpp
+++ Recursion/practice/finonacci.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 using namespace std;
+int fibo(int n){
+    //base case
+    if(n == 0){
+        return 0;
+    }
+    if(n == 1){
+        return 1;
+    }
+
+    //recursive relation
+    int fib = fibo(n-1)+fibo(n-2);
+    
+    return fib;
+
+}
 
 int main (){
-    
+    int n;
+    cout<<"enter no :";
+    cin>>n;
+
+    cout<<"fibonacci for is :"<<fibo(n);
 
 
     return 0;
