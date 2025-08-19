d1d7dddca8f277aa8c0a11d4317c05112dc47471
ayush01-dev <111ayushh@gmail.com>
2025-08-19 23:09:46 +0530

    Implement merge sort algorithm with helper function for merging two sorted arrays; add main function for testing

--- DnC/algoMergeSort.cpp
+++ DnC/algoMergeSort.cpp
@@ -1,8 +1,99 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+void mergeTwoSortedArray(int* arr,int s , int e){
+    int mid = s+(e-s)/2;
 
+    int len1 = mid - s + 1;
+    int len2 =  e - mid;
+
+    int* left = new int[len1];
+    int* right = new int[len2];
+
+    //copy values into array
+    int k = s;
+    for(int i = 0; i<len1 ; i++){
+        left[i] = arr[k];
+        k++;
+
+        
+    }
     
+    
+
+    k = mid + 1;
+    for (int i = 0; i<len2 ; i++){
+        right[i] = arr[k];
+        k++;
+    }
+
+
+
+    //merge logic
+    int leftindex = 0;
+    int rightindex = 0;
+    int mainArrayIndex = s;
+
+    while(leftindex < len1 && rightindex <len2){
+        if (left[leftindex] < right[rightindex]){
+            arr[mainArrayIndex++] = left[leftindex++];
+
+        }
+        else {
+            arr[mainArrayIndex++] = right[rightindex++];
+        }
+    }
+
+    //copy logic for remaining elements left
+    while(leftindex < len1){
+        arr[mainArrayIndex++] = left[leftindex++];
+
+    }
+    //copy logci for right arry
+    while(rightindex < len2){
+        arr[mainArrayIndex++] = right[rightindex++];
+    }
+
+
+    //TODO: delete left and right newly created array
+
+}
+
+void  mergesort(int* arr, int s, int e){
+    //base case
+    //s == e (single element left in array)
+    //s > e (invalid element)
+    if(s >= e){
+        return;
+    }
+
+    int mid = s+(e-s)/2;
+
+    //left part sort kar do recursion bhaiya
+    mergesort(arr,s,mid);
+
+    //right part sort kar do recursion bhaiya
+    mergesort(arr,mid+1,e);
+   
+    //merge two sorted array
+    mergeTwoSortedArray(arr,s,e);
+}
+
+int main(){
+    int arr[] = {4,14,0,3,89};
+    int n = 5;
+    int s = 0;
+    int e = n-1;
+
+    //calling mergesort function  
+    mergesort(arr,s,e);
+
+
+    //printing the array
+    for (int i = 0; i < n ; i ++){
+        cout<<arr[i]<<" ";
+    }
+
+
     return 0;
 }
